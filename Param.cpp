3fbc41a05e46492ea79ed15feffc87470ccf1979
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-02-05 14:18:45 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -59,7 +59,7 @@ Param::Param() {
 	nOutput = 10;     // # of neurons in output layer
 	alpha1 = l/100.0;	// Learning rate for the weights from input to hidden layer
 	alpha2 = l/2.0/100.0;	// Learning rate for the weights from hidden to output layer
-	dalpha = dl/100;
+	dalpha = l/100;
 	nalpha1 = l/100;
 	nalpha2 = l/2.0/100;
 	tp = 12.5;
