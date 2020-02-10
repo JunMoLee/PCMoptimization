2309908928da1f4a63816ebd59b5eef298b4cfa9
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-02-10 13:04:24 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -45,11 +45,11 @@ Param::Param() {
 	numMnistTrainImages = 60000;// # of training images in MNIST
 	numMnistTestImages = 10000;	// # of testing images in MNIST
 	const double
-        l=20;
+        l=60;
 	const double
 	ll=20;
 	const double
-	dl= 20;
+	dl=700;
 	/* Algorithm parameters */
 	numTrainImagesPerEpoch = 8000;	// # of training images per epoch
 	totalNumEpochs = 125;	// Total number of epochs
@@ -59,12 +59,12 @@ Param::Param() {
 	nOutput = 10;     // # of neurons in output layer
 	alpha1 = l/100.0;	// Learning rate for the weights from input to hidden layer
 	alpha2 = l/2.0/100.0;	// Learning rate for the weights from hidden to output layer
-	dalpha = l/100;
+	dalpha = dl/100;
 	nalpha1 = l/100;
 	nalpha2 = l/2.0/100;
 	tp = 12.5;
 	tn = 12.5;
-	kp=64;
+	kp=128;
 	kd=2;
 	knp=97;
 	knd=97;
